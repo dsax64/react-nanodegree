08dac8338f44fa86d0271333e52194cee4ee8b2f
Balazs Szele <balazs.szele@prezi.com>
2018-02-01 20:25:01 -0800

    Add create contact functionality

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -22,6 +22,14 @@ class App extends Component {
         ContactsAPI.remove(contact)
     };
 
+    createContact = (contact) => {
+        ContactsAPI.create(contact).then( contact => {
+            this.setState( state => ({
+                contact: state.contacts.concat([ contact ])
+            }))
+        })
+    }
+
     render() {
         return (
             <div className="app">
@@ -31,7 +39,14 @@ class App extends Component {
                         contacts={this.state.contacts}
                     />
                 )}/>
-                <Route path="/create" component={CreateContact}/>
+                <Route path="/create" render={({ history }) => (
+                    <CreateContact
+                        onCreateContact={(contact) => {
+                            this.createContact(contact)
+                            history.push('/')
+                        }}
+                    />
+                )}/>
             </div>
         )
     }
