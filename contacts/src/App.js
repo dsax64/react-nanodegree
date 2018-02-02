b579b9c0f760b593a8b87c0eceb93057aaa2e22c
Balazs Szele <balazs.szele@prezi.com>
2018-02-01 18:56:43 -0800

    Add navigation with react router

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
 import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
@@ -6,8 +7,7 @@ import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
     state = {
-        contacts : [],
-        screen: 'list' // list, create
+        contacts : []
     };
 
     componentDidMount() {
@@ -25,18 +25,13 @@ class App extends Component {
     render() {
         return (
             <div className="app">
-                {this.state.screen === 'list' && (
+                <Route exact path="/" render={() => (
                     <ListContacts
                         onDeleteContact={this.removeContact}
                         contacts={this.state.contacts}
-                        onNavigate={() => {
-                            this.setState({ screen: 'create' })
-                        }}
                     />
-                )}
-                {this.state.screen === 'create' && (
-                    <CreateContact/>
-                )}
+                )}/>
+                <Route path="/create" component={CreateContact}/>
             </div>
         )
     }
--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
@@ -22,7 +23,7 @@ class ListContacts extends React.Component{
     };
 
     render() {
-        const { contacts, onDeleteContact, onNavigate} = this.props;
+        const { contacts, onDeleteContact} = this.props;
         const { query } = this.state;
 
         let showingContacts;
@@ -45,11 +46,10 @@ class ListContacts extends React.Component{
                         value={query}
                         onChange={(event) => this.updateQuery(event.target.value)}
                     />
-                    <a
-                        href="#create"
-                        onClick={onNavigate}
+                    <Link
+                        to="/create"
                         className="add-contact"
-                    >Add Contact</a>
+                    >Add Contact</Link>
 
                 </div>
                 {showingContacts.length !== contacts.length && (
