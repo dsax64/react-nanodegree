93e66b811b02ddbb8b645a390a7e06a0ad8bc178
Balazs Szele <balazs.szele@prezi.com>
2018-04-01 09:59:05 -0700

    Finish up udacimeals

---
+++ udacimeals/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducer from './reducers'
+import { Provider } from 'react-redux';
+
+
+const logger = store => next => action => {
+      console.group(action.type)
+      console.info('dispatching', action)
+      let result = next(action)
+      console.log('next state', store.getState())
+      console.groupEnd(action.type)
+      return result
+}
+
+const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+    reducer,
+    composeEnchancers(
+        applyMiddleware(logger),
+    )
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+registerServiceWorker();
